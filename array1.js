b9e97a3c5951f168b930cd5e4c6752f1da01cfd3
willian <willian.chris.89@gmail.com>
2022-02-08 11:02:48 -0300

    exercícios 1 de array em execução

--- array1.js
+++ array1.js
@@ -5,24 +5,49 @@ function Medias() {
         { nome: "Samantha", prova: { p1: 8, p2: 9.2, p3: 7 } },
         { nome: "Andreia", prova: { p1: 9, p2: 8, p3: 10 } },
         { nome: "Raquel", prova: { p1: 10, p2: 9.7, p3: 5 } },
-        { nome: "Inês Brasil", prova: { p1: 2, p2: 4.6, p3: 9.9 } },
+        { nome: "Amanda", prova: { p1: 2, p2: 4.6, p3: 9.9 } },
         { nome: "Pietra", prova: { p1: 8.3, p2: 3.1, p3: 9.8 } },
         { nome: "Jaqueline", prova: { p1: 3.4, p2: 7.2, p3: 6.8 } },
         { nome: "Alessandra", prova: { p1: 1.4, p2: 2.7, p3: 6.9 } },
-        { nome: "Beyoncé", prova: { p1: 7, p2: 5.5, p3: 9.1 } },
+        { nome: "Jane Kelly", prova: { p1: 7, p2: 5.5, p3: 9.1 } },
     ]
+    const separacao = alunas.map(aluna => aluna.prova.p1)
+    const separacao2 = alunas.map(aluna => aluna.prova.p2)
+    const separacao3 = alunas.map(aluna => aluna.prova.p3)
 
-    const separa = alunas.map(aluna => aluna.prova.p1)
-    const separa2 = alunas.map(aluna => aluna.prova.p2)
-    const separa3 = alunas.map(aluna => aluna.prova.p3)
-    let resultado = (separa[0] + separa2[0] + separa3[0]) / 3
-    console.log(resultado.toFixed(1))
-
+    
 
 
+    let soma1 = separacao.reduce(function (valor1, valor2) {
+        return valor1 + valor2;
+    })
+    let soma2 = separacao2.reduce(function (valor2,valorX) {
+        return valor2 + valorX;
+    })
+   let soma3 = separacao3.reduce(function (valor3,valorY) {
+     return valor3 + valorY
+ })
+const somaTotal = (soma1 + soma2 + soma3) /3 
+console.log(somaTotal.toFixed(1))
 }
 
 Medias()
+
+
+/*Objeto
+var obj = {a:1, b:2, c:3};
+
+//Para prop (propriedade) in obj (objeto) faça
+for (var prop in obj) {
+  // ctrl+shift+k (para abrir o console no mozilla firefox)
+  console.log("obj." + prop + " = " + obj[prop]);
+}
+*/
+
+
+
+
+
 
 /* if (vegetais.indexOf(vegetal) === -1) {
     vegetais.push(vegetal);

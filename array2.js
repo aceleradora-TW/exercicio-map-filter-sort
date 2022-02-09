67b5e89323d584d3cd955713694140d6af0289a6
willian <willian.chris.89@gmail.com>
2022-02-09 14:22:14 -0300

    implementação do filter no exercício 2

--- array2.js
+++ array2.js
@@ -1,13 +1,74 @@
-const alunas = [
-    { nome: "Ashley", prova: { p1: 5.6, p2: 6.7, p3: 9 } },
-    { nome: "Sabrina", prova: { p1: 6.3, p2: 7.5, p3: 10 } },
-    { nome: "Samantha", prova: { p1: 8, p2: 9.2, p3: 7 } },
-    { nome: "Andreia", prova: { p1: 9, p2: 8, p3: 10 } },
-    { nome: "Raquel", prova: { p1: 10, p2: 9.7, p3: 5 } },
-    { nome: "Amanda", prova: { p1: 2, p2: 4.6, p3: 9.9 } },
-    { nome: "Pietra", prova: { p1: 8.3, p2: 3.1, p3: 9.8 } },
-    { nome: "Jaqueline", prova: { p1: 3.4, p2: 7.2, p3: 6.8 } },
-    { nome: "Alessandra", prova: { p1: 1.4, p2: 2.7, p3: 6.9 } },
-    { nome: "Jane Kelly", prova: { p1: 7, p2: 5.5, p3: 9.1 } },
-]
+function aprovadas() {
+    const alunas = [
+        { nome: "Ashley", prova: { p1: 5.6, p2: 6.7, p3: 9 } },
+        { nome: "Sabrina", prova: { p1: 6.3, p2: 7.5, p3: 10 } },
+        { nome: "Samantha", prova: { p1: 8, p2: 9.2, p3: 7 } },
+        { nome: "Andreia", prova: { p1: 9, p2: 8, p3: 10 } },
+        { nome: "Raquel", prova: { p1: 10, p2: 9.7, p3: 5 } },
+        { nome: "Amanda", prova: { p1: 2, p2: 4.6, p3: 9.9 } },
+        { nome: "Pietra", prova: { p1: 8.3, p2: 3.1, p3: 9.8 } },
+        { nome: "Jaqueline", prova: { p1: 3.4, p2: 7.2, p3: 6.8 } },
+        { nome: "Alessandra", prova: { p1: 1.4, p2: 2.7, p3: 6.9 } },
+        { nome: "Jane Kelly", prova: { p1: 7, p2: 5.5, p3: 9.1 } },
+    ]
+    const separaNome = alunas.map(aluna => aluna.nome)
+    //console.log(separaNome)
+    const separacao = alunas.map(aluna => aluna.prova.p1)
+    const separacao2 = alunas.map(aluna => aluna.prova.p2)
+    const separacao3 = alunas.map(aluna => aluna.prova.p3)
+    function filtro(value) {
+        if (soma1 >= 6) {
+            return separaNome
+        }
+        let nomesFiltrados = separaNome.filter(filtro)
 
+    }
+    const soma1 = (separacao[0] + separacao2[0] + separacao3[0]) / 3
+    const soma2 = (separacao[1] + separacao2[1] + separacao3[1]) / 3
+    const soma3 = (separacao[2] + separacao2[2] + separacao3[2]) / 3
+    const soma4 = (separacao[3] + separacao2[3] + separacao3[3]) / 3
+    const soma5 = (separacao[4] + separacao2[4] + separacao3[4]) / 3
+    const soma6 = (separacao[5] + separacao2[5] + separacao3[5]) / 3
+    const soma7 = (separacao[6] + separacao2[6] + separacao3[6]) / 3
+    const soma8 = (separacao[7] + separacao2[7] + separacao3[7]) / 3
+    const soma9 = (separacao[8] + separacao2[8] + separacao3[8]) / 3
+    const soma10 = (separacao[9] + separacao2[9] + separacao3[9]) / 3
+
+
+    filtro()
+    //mapeamento das pessoas 
+
+}
+console.log(aprovadas())
+
+
+/*
+soma1 || soma2 || soma3 || soma4 || soma5 || soma6 || soma7 || soma8 || soma9 || soma10 >= 6
+
+if (soma2 >= 6) {
+       return "Sabrina"
+   }
+   if (soma3 >= 6) {
+       return "Samantha"
+   }
+   if (soma4 >= 6) {
+       return "Andreia"
+   }
+   if (soma5 >= 6) {
+       return "Raquel"
+   }
+   if (soma6 >= 6) {
+       return "Amanda"
+   }
+   if (soma7 >= 6) {
+       return "Pietra"
+   }
+   if (soma8 >= 6) {
+       return "Jaqueline"
+   }
+   if (soma9 >= 6) {
+       return "Alessandra"
+   }
+   if (soma10 >= 6) {
+       return "Jane Kelly"
+   }*/

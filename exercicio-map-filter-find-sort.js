37a22e4f0fc0f5bd1a4d7254c0ca1c6943e18730
Andressa Barcelos <andressa.barcelos26@gmail.com>
2022-05-09 13:40:42 -0300

    Exercicio-map-filter-find-sort em andamento

--- exercicio-map-filter-find-sort.js
+++ exercicio-map-filter-find-sort.js
@@ -11,3 +11,51 @@ const alunas = [
     { nome: "Alessandra", prova: { p1: 1.4, p2: 2.7, p3: 6.9 } },
     { nome: "Jane Kelly", prova: { p1: 7, p2: 5.5, p3: 9.1 } },
 ]
+
+const mediaAlunas = alunas.map((aluna) => {
+    let mediaAlunas = (aluna.prova.p1 + aluna.prova.p2 + aluna.prova.p3) / 3
+    return parseFloat(mediaAlunas.toFixed(1))
+});
+console.log(mediaAlunas)
+
+const buscaAlunasAprovadas = () => {
+    const alunasAprovadas = alunas.filter((aluna, indice) => mediaAlunas[indice] >= 7)
+    const nomesAlunasAprovadas = alunasAprovadas.map((aluna) => aluna.nome)
+    return nomesAlunasAprovadas
+}
+console.log(buscaAlunasAprovadas(alunas))
+
+const buscaAlunasReprovadas = () => {
+    const alunasReprovadas = alunas.filter((aluna, indice) => mediaAlunas[indice] < 7)
+    const nomesAlunasReprovadas = alunasReprovadas.map((aluna) => aluna.nome)
+    return nomesAlunasReprovadas
+}
+console.log(buscaAlunasReprovadas(alunas))
+
+const listaDeAlunas = alunas.map((aluna, indice) => {
+    let aprovada = true
+    if(mediaAlunas[indice] >= 7){
+        aprovada = true
+    } else {
+        aprovada = false
+    }
+    const alunaObj = {
+        nome: aluna.nome,
+        media: mediaAlunas[indice],
+        aprovada: aprovada
+    }
+    return alunaObj 
+})
+console.log(listaDeAlunas)
+
+// console.log(listaDeAlunas(alunas))
+
+// const listaDeAlunas = () => {
+//     const listaAlunas = alunas.map((aluna) => {
+//         const alunaObj = {
+//             nome: aluna.nome
+//         }
+//         return alunaObj 
+//     })
+//     console.log(listaAlunas) 
+// }
